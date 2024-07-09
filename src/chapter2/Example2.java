9212f60ad13fb912e573b647e916d1087635cb4b
taron <t.mkrtchyan@inorain.com>
2024-07-09 21:49:39 +0400

    add files

---
+++ src/chapter2/Example2.java
@@ -0,0 +1,11 @@
+package chapter2;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int num;
+        num = 100;
+        System.out.println("num : " + num);
+        num = num * 2;
+        System.out.println(num);
+    }
+}
---
+++ src/chapter2/IfSample.java
@@ -0,0 +1,21 @@
+package chapter2;
+
+public class IfSample {
+    public static void main(String[] args) {
+        int x,y;
+        x = 10;
+        y = 20;
+
+        if (x < y){
+            System.out.println("x < y");
+        }
+        x =  x * 2;
+        if (x == y) {
+            System.out.println("x == y");
+        }
+        x  = x * 2;
+        if (x > y){
+            System.out.println("x > y");
+        }
+    }
+}
